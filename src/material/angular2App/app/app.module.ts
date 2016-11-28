2fe5c2af830469d569da596f4e97732ae54eff5b
manuel <manuel.moya.roldan@gmail.com>
2016-11-28 16:09:01 +0100

    --

---
+++ src/material/angular2App/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { MaterialModule } from '@angular/material';
+
+import { Configuration } from './app.constants';
+import { routing } from './app.routes';
+
+import { NAV_DROPDOWN_DIRECTIVES } from './shared/nav-dropdown.directive';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+import { TestDataService } from './services/testDataService';
+
+@NgModule({
+    imports: [
+        MaterialModule.forRoot(),
+        BrowserModule,
+        CommonModule,
+        FormsModule,
+        routing,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        NAV_DROPDOWN_DIRECTIVES,
+        AboutComponent,
+        HomeComponent
+    ],
+    providers: [
+        TestDataService,
+        Configuration
+    ],
+    bootstrap: [AppComponent],
+})
+
+export class AppModule { }
---
+++ src/material/angular2App/vendor.ts
@@ -0,0 +1,8 @@
+﻿//import 'jquery/src/jquery';
+//import 'bootstrap/dist/js/bootstrap';
+
+//import './css/bootstrap.css';
+//import './css/bootstrap-theme.css';
+import './css/assets/style.css';
+import './css/assets/css/style.css';
+
