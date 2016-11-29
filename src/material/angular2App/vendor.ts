1836b0f7b5ade5a13ea0737c090c673daddd0847
manuel <manuel.moya.roldan@gmail.com>
2016-11-29 17:41:00 +0100

    --20161129

--- src/material/angular2App/vendor.ts
+++ src/material/angular2App/vendor.ts
@@ -3,6 +3,6 @@
 
 //import './css/bootstrap.css';
 //import './css/bootstrap-theme.css';
-import './css/assets/style.css';
+//import './css/assets/style.css';
 import './css/assets/css/style.css';
 
